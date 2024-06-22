0edfd55258963460d0d5f72758151d0cbebdb9e1
S-Sajith <sajith@dsiroot.org>
2024-06-22 12:54:23 +0530

    Handle case where access token is expired using axios interceptors

--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -1,56 +1,79 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 const SPOTIFY_BASE_URL = 'https://api.spotify.com/v1';
+const ACCESS_TOKEN_KEY = 'spotify_access_token';
 
 const getAccessToken = (): string | null => {
-  return localStorage.getItem('spotify_access_token');
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
 };
 
-export const getUserProfile = async (): Promise<{ id: string }> => {
+const removeAccessToken = () => {
+  localStorage.removeItem(ACCESS_TOKEN_KEY);
+};
+
+const axiosInstance = axios.create({
+  baseURL: SPOTIFY_BASE_URL,
+});
+
+axiosInstance.interceptors.request.use(config => {
   const token = getAccessToken();
-  const response = await axios.get(`${SPOTIFY_BASE_URL}/me`, {
-    headers: {
-      Authorization: `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      removeAccessToken();
+      window.location.href = '/';
+      return Promise.reject(error);
     }
-  });
-  return response.data;
+    return Promise.reject(error);
+  }
+);
+
+export const getUserProfile = async (): Promise<{ id: string }> => {
+  try {
+    const response = await axiosInstance.get('/me');
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
 
 export const getUserTopTracks = async (timeRange: string): Promise<any[]> => {
-  const token = getAccessToken();
-  const response = await axios.get(`${SPOTIFY_BASE_URL}/me/top/tracks`, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    params: {
-      limit: 50,
-      time_range: timeRange
-    }
-  });
-  return response.data.items;
+  try {
+    const response = await axiosInstance.get('/me/top/tracks', {
+      params: {
+        limit: 50,
+        time_range: timeRange,
+      },
+    });
+    return response.data.items;
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
 
 export const createPlaylist = async (userId: string, name: string, trackUris: string[]): Promise<string> => {
-  const token = getAccessToken();
-  const playlistResponse = await axios.post(`${SPOTIFY_BASE_URL}/users/${userId}/playlists`, {
-    name: name,
-    description: 'Playlist created by SpottyPlaylist',
-    public: false
-  }, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+  try {
+    const playlistResponse = await axiosInstance.post(`/users/${userId}/playlists`, {
+      name: name,
+      description: 'Playlist created by SpottyPlaylist',
+      public: false,
+    });
 
-  const playlistId = playlistResponse.data.id;
+    const playlistId = playlistResponse.data.id;
 
-  await axios.post(`${SPOTIFY_BASE_URL}/playlists/${playlistId}/tracks`, {
-    uris: trackUris
-  }, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+    await axiosInstance.post(`/playlists/${playlistId}/tracks`, {
+      uris: trackUris,
+    });
 
-  return playlistId;
+    return playlistId;
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
