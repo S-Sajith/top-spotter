39cd00a3229940d2f18e8ac53592250180ee07e0
S-Sajith <sajith@dsiroot.org>
2024-08-24 14:29:00 +0530

    Design update

--- src/pages/HeroPage.tsx
+++ src/pages/HeroPage.tsx
@@ -1,3 +1,6 @@
+import SpotifyLogo from "../assets/Spotify_Logo_CMYK_White.png";
+import spotifyIcon from "../assets/Spotify_Icon_CMYK_White.png";
+
 const HeroPage = () => {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const CLIENT_ID = "5f5e619c62b942069c1ce057f7df59df";
@@ -16,17 +19,33 @@ const HeroPage = () => {
 
   return (
     <div className="flex flex-col justify-center items-center h-full text-center md:text-start">
-      <div className="font-semibold text-white text-3xl md:text-5xl mb-8 w-7/12 mt-[20vh] md:mt-[25vh]">
+      <div
+        className="font-semibold text-white mb-8 w-7/12 mt-[20vh] md:mt-[25vh]"
+        style={{ fontSize: "clamp(2rem, 5vw, 3rem)", lineHeight: "1.2" }}
+      >
         Login to discover your{" "}
-        <span className="text-brandPurple">most played</span>{" "}
-        <span className="text-brandPurple">Spotify </span>tracks, and save them
-        straight into a <span className="text-brandPurple">playlist</span>.
+        <span className="text-brandPurple">most played</span> tracks, and save
+        them straight into a <span className="text-brandPurple">playlist</span>{" "}
+        on{" "}
+        <img
+          src={SpotifyLogo}
+          alt="Spotify Logo"
+          className="inline-block align-middle mt-1.5 md:mt-0"
+          style={{ height: "70px", width: "auto" }}
+        />
       </div>
+
       <button
         onClick={handleLogin}
-        className="bg-brandPurple text-white p-4 rounded-full text-lg font-semibold md:self-start md:ml-[20vw]"
+        className="bg-brandPurple text-white p-4 rounded-full flex items-center md:self-start md:ml-[20vw]"
       >
-        Connect to Spotify
+        <img
+          src={spotifyIcon}
+          alt="Spotify logo"
+          className="inline-block w-4 h-4 mr-1"
+          style={{ width: "25px", height: "25px" }}
+        />
+        <span className="text-lg font-semibold">Connect to Spotify</span>
       </button>
     </div>
   );
